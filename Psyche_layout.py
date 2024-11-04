2f44897b0a5b680eb9b379fd92d45f6502abd553
szczotar <artur.szczotarski@mindboxgroup.com>
2024-11-04 23:05:37 +0100

    generate_template

--- Psyche_layout.py
+++ Psyche_layout.py
@@ -1,6 +1,14 @@
 import tkinter
 import tkinter.messagebox
 import customtkinter
+from customtkinter import CTkImage
+from PIL import Image
+from datetime import datetime
+from docxtpl import DocxTemplate
+import jinja2
+
+tpl = DocxTemplate(r'C:\Users\ArturSzczotarski\psyche\RODO_template.DOCX')
+
 
 customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
@@ -11,9 +19,10 @@ class App(customtkinter.CTk):
         super().__init__()
 
         # configure window
-        self.title("CustomTkinter complex_example.py")
-        self.geometry(f"{1100}x{580}")
+        self.title("Psyche Forms")
+        self.geometry(f"{900}x{580}")
 
+        my_image = CTkImage(light_image=Image.open(r"C:\Users\ArturSzczotarski\psyche\Dominika.jpg"), size=(200,250))
         # configure grid layout (4x4)
         self.grid_columnconfigure(1, weight=1)
         self.grid_columnconfigure((2, 3), weight=0)
@@ -23,7 +32,7 @@ class App(customtkinter.CTk):
         self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
         self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
         self.sidebar_frame.grid_rowconfigure(4, weight=1)
-        self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="CustomTkinter", font=customtkinter.CTkFont(size=20, weight="bold"))
+        self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="Psyche", font=customtkinter.CTkFont(size=20, weight="bold"))
         self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
         self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event)
         self.sidebar_button_1.grid(row=1, column=0, padx=20, pady=10)
@@ -47,35 +56,82 @@ class App(customtkinter.CTk):
         # self.textbox = customtkinter.CTkTextbox(self, width=250)
         # self.textbox.grid(row=0, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
 
+        #Photo 
+        # self.photo = customtkinter.CTkLabel(self, image=my_image, text = "", font=customtkinter.CTkFont(size=20, weight="bold"))
+        # self.photo.grid(row=1, column=1, padx=0, pady=0)
+
         # create tabview
         self.tabview = customtkinter.Form1(self)
-        self.tabview.grid(row=0, column=1, padx=10, pady=(20, 0), sticky="nsew")
+        self.tabview.grid(row=0, column=1, padx=10, pady=(20, 0), sticky="ew")
         self.tabview.add("Form1")
         self.tabview.add("Form2")
         
         # Form1 inside
-        self.tabview.tab("Form1").grid_columnconfigure(0, weight=1)  # configure grid of individual tabs
+        self.tabview.tab("Form1").grid_columnconfigure((0,1,2,3), weight=1)  # configure grid of individual tabs
+        self.tabview.tab("Form1").grid_rowconfigure((0,1,2,3,4,5,6,7), weight=0)
 
+
+        #Name
         self.nameLabel = customtkinter.CTkLabel(self.tabview.tab("Form1"),
-									text="Name")
-        
+									text="Imię")
         self.nameLabel.grid(row=0, column=0,
-							padx=100, pady=20,
-							sticky="nsew")
-        
+							padx=10, pady=10,
+							sticky="ew",
+                              )
         self.nameEntry = customtkinter.CTkEntry(self.tabview.tab("Form1"),
-						placeholder_text="Teja")
+						placeholder_text="Jan")
         self.nameEntry.grid(row=0, column=1,
-							columnspan=3, padx=100,
-
-							pady=20, sticky="ew")
-        
+							columnspan=2, padx=10,
+							pady=10, sticky="ew")      
+        #Last name
         self.lastnameLabel = customtkinter.CTkLabel(self.tabview.tab("Form1"),
-									text="LastName")
-        
+									text="Nazwisko") 
         self.lastnameLabel.grid(row=1, column=0,
-							padx=0, pady=20,
-							sticky="nsew")
+							padx=10, pady=10,
+							sticky="ew")
+        self.lasNameEntry = customtkinter.CTkEntry(self.tabview.tab("Form1"),
+						placeholder_text="Kowalski")
+        self.lasNameEntry.grid(row=1, column=1,
+							columnspan=2, padx=10,
+							pady=10, sticky="ew")       
+        #PESEL
+        self.peselLabel = customtkinter.CTkLabel(self.tabview.tab("Form1"),
+									text="PESEL")
+        self.peselLabel.grid(row=2, column=0,
+							padx=10, pady=10,
+							sticky="ew")
+        self.peselEntry = customtkinter.CTkEntry(self.tabview.tab("Form1"),
+						placeholder_text="000000")
+        self.peselEntry.grid(row=2, column=1,
+							columnspan=2, padx=10,
+							pady=10, sticky="ew")
+        #Tele
+        self.phoneLabel = customtkinter.CTkLabel(self.tabview.tab("Form1"),
+									text="Telefon")
+        self.phoneLabel.grid(row=4, column=0,
+							padx=10, pady=10,
+							sticky="ew")
+        self.phoneEntry = customtkinter.CTkEntry(self.tabview.tab("Form1"),
+						placeholder_text="00000")
+        self.phoneEntry.grid(row=4, column=1,
+							columnspan=2, padx=10,
+							pady=10, sticky="ew")
+        #Email
+        self.emailLabel = customtkinter.CTkLabel(self.tabview.tab("Form1"),
+									text="e-mail")
+        self.emailLabel.grid(row=5, column=0,
+							padx=10, pady=10,
+							sticky="ew")
+        self.emailEntry = customtkinter.CTkEntry(self.tabview.tab("Form1"),
+						placeholder_text="jan.kowalski@gmai.com")
+        self.emailEntry.grid(row=5, column=1,
+							columnspan=2, padx=10,
+							pady=10, sticky="ew")
+        
+        #generate button
+        self.generate_button1 = customtkinter.CTkButton(self.tabview.tab("Form1"), text="Generuj" , command=self.generate_template_rodo)
+        self.generate_button1.grid(row=6, column=0, padx=20, pady=50)
+
         
         # self.optionmenu_1 = customtkinter.CTkOptionMenu(self.tabview.tab("Form1"), dynamic_resizing=False,
         #                                                 values=["Value 1", "Value 2", "Value Long Long Long"])
@@ -113,6 +169,29 @@ class App(customtkinter.CTk):
 
     def sidebar_button_event(self):
         print("sidebar_button click")
+
+    def generate_template_rodo(self):
+
+        name = self.nameEntry.get()
+        lastName =  self.lasNameEntry.get()
+        pesel = self.peselEntry.get()
+        phone = self.phoneEntry.get()
+        email = self.emailEntry.get()
+
+        context = {
+            "pesel" : pesel,
+            "name" : name,
+            "last_name" : lastName,
+            "date" : str(datetime.strftime(datetime.today(),'%d-%m-%Y')),
+            "email" : email,
+            "tel" :phone
+            }
+
+        jinja_env = jinja2.Environment(autoescape=True)
+        tpl.render(context, jinja_env)
+        tpl.save(fr"C:\Users\ArturSzczotarski\psyche\Rodo_{name}.DOCX")
+
+        
 
 
 if __name__ == "__main__":
